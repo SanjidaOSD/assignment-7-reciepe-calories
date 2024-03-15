7d54c25e2394320e1653dc0126936021a014ae1d
unknown <sanjidaema81@gmail.com>
2024-03-16 00:58:12 +0600

    creating chefs table project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <h1 className='text-2xl font-bold bg-red-600'>React tailwind</h1>
+      <button class="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
