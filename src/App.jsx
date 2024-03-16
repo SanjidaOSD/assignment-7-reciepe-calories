ee4c0d5fb929274a7d6a4857042c6ae07dda8a73
unknown <sanjidaema81@gmail.com>
2024-03-16 13:24:21 +0600

    created dynamic value

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,53 @@
 import './App.css'
 import Header from './Components/Header/Header'
 
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Recipe from './Components/Recipe'
+
 
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [cart, setCart] = setCart([]);
 
+
+  useEffect(() =>{
+    fetch('./recipeData.json')
+    .then(res =>res.json())
+    .then(data => {
+      setRecipes(data);
+    })
+  },[])
+
+const handleCart = (cart) => {
+  const isExist = cart.find(recipe => recipe.id == cart.id);
+  console.log(isExist)
+}
   return (
     <>
-    <Header></Header>
-     
-  
+      <Header></Header>
+
+      <div className="main-container lg:flex lg:justify-around lg:p-10 mt-16 lg:mt-0 ">
+        <div className="cards-container lg:grid lg:grid-cols-2 gap-2">
+          {
+            recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} handleCart={handleCart} ></Recipe>)
+          }
+         
+        </div>
+        {/* carts container */}
+        <div className=" lg:w-3/5 shadow-2xl p-4">
+          <h1 className='text-xl font-bold text-center mb-2 underline'>Want to cook: { }</h1>
+          <div className='border h-0 w-full bg-black'></div>
+          <div className=' flex justify-around gap-12'>
+            <p>Name</p>
+            <p>Time</p>
+            <p>Calories</p>
+          </div>
+        </div>
+      </div>
+
+
     </>
   )
 }
---
+++ src/Components/Recipe.jsx
@@ -0,0 +1,54 @@
+import minImg from '../assets/min.png'
+import calImg from '../assets/cal.png'
+import PropTypes from 'prop-types'
+const Recipe = ({recipe, handleCart}) => {
+  const {id,img,name,description,ingredients,calories,time} = recipe;
+    // console.log(recipe.name)
+    return (
+        <div className='grid grid-cols-2 gap-2'>
+             <div className="card w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img key={id} src={img} alt="Shoes" className="rounded-xl w-80 h-48" />
+            </figure>
+            {/* <div className="card-body items-center text-center"> */}
+            <div className='ml-12'>
+              <h2 className="card-title text-center text-xl font-bold mb-3">{name}</h2>
+              <p className='mb-3'>{description}</p>
+              <div className='border h-0 w-full bg-black'></div>
+              <div>
+                <h1 className='text-xl font-bold mb-3'>Ingredients:{ingredients.length}</h1>
+               
+               <div className='items-left mb-3'>
+               <li>{ingredients[0]}</li>
+                <li>{ingredients[1]}</li>
+                <li>{ingredients[2]}</li>
+               </div>
+                
+              </div>
+              <div className='border h-0 w-full bg-black'></div>
+
+              <div className='flex gap-12 mb-3'>
+                <div className='flex'>
+                  <img src={minImg} alt="" />
+                  <p>{time} minuits</p>
+                </div>
+                <div className='flex'>
+                  <img src={calImg} alt="" />
+                  <p>{calories}Calories</p>
+
+                </div>
+              </div>
+              <div className="card-actions mt-4">
+                <button onClick={()=>handleCart(recipe)} className="btn bg-green-500">Want to Cook</button>
+              </div>
+            </div>
+          </div>
+         </div>
+    );
+};
+
+Recipe.propTypes = {
+    recipe:PropTypes.object.isRequired
+}
+
+export default Recipe;
